10fc8f73a5f257d4057e9086851a119872999d0e
Cookie-Kumano <Cookie-Kumano@users.noreply.github.com>
2019-08-17 10:20:21 +0900

    changed camera pos

--- Assets/Scripts/ShipAccel.cs
+++ Assets/Scripts/ShipAccel.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraControl : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
