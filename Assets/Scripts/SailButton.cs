52a04b2f868ef9f5233abbfdb1d8eacae1e5a5b2
shigure1007 <54182642+shigure1007@users.noreply.github.com>
2019-08-17 15:08:27 +0900

    chenged script
    かえたよー

---
+++ Assets/Scripts/SailButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SailButton : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject Canvas;
+    public GameObject Sail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onSailButtonCricked()
+    {
+        Canvas.SetActive(false);
+        Sail.SetActive(true);
+    }
+}
